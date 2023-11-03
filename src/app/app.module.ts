63ba8c0efa6c384b61c7d16504931ecdb8147716
Aleksandr Harisov <bwb-a.harisov@fondespierre.com>
2023-11-03 14:21:05 +0100

    created app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { TeamComponent } from './components/team/team.component';
+import { UsersComponent } from './components/users/users.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    TeamComponent,
+    UsersComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
