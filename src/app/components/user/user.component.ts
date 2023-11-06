d83c01b27077e9d0cdd53bafc9b38b5f34782a5b
Aleksandr Harisov <bwb-a.harisov@fondespierre.com>
2023-11-06 09:30:05 +0100

    modified team and user buttons

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -12,18 +12,24 @@ import { Button } from 'src/app/interfaces/button';
 export class UserComponent implements OnInit {
 
   @Input() user?: User;
+  @Input() inTeam?: boolean;
   @Output() addToTeam = new EventEmitter<User>();
+  @Output() removeFromTeam = new EventEmitter<User>();
 
+  isSelected: boolean = false;
+  isInTeam: boolean = false;
   addButton: Button = {};
 
   ngOnInit(): void {
     
     // console.log(this.user);
     this.addButton = {
-      title: 'Ajoute dans l\'equipe',
+      title: 'Ajoutez dans l\'equipe',
       titleSelected: 'Déjà dans l\'equipe',
+      titleInTeam: 'Retirez de l\'equipe',
       class: 'w-100 btn btn-primary',
       classSelected: 'w-100 btn btn-success',
+      classToRemove: 'w-100 btn btn-danger',
       user: this.user
     }
   }
@@ -31,7 +37,13 @@ export class UserComponent implements OnInit {
   addNewUser(user: User): void {
 
     // this.newEvent.emit(user);
-    console.log('clicked', this.addButton)
+    console.log('clicked', this.addButton);
+    this.isSelected = true;
     this.addToTeam.emit(user);
+  }
+
+  removeUser(user: User) {
+
+    this.removeFromTeam.emit(user);
   }
 }
--- src/app/interfaces/button.ts
+++ src/app/interfaces/button.ts
@@ -2,7 +2,9 @@
 export interface Button {
     title?: string,
     titleSelected?: string,
+    titleInTeam?: string,
     class?: string,
     classSelected?: string,
+    classToRemove?: string,
     user?: any
 }
